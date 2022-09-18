da8aa5fc3733277f88cb49f89be393e9ae60a2f7
Justin-mario <79143260+Justin-mario@users.noreply.github.com>
2022-09-18 16:39:35 +0100

    initial commit

---
+++ src/test/java/com/coronavirustrackersite/CoronaVirusTrackerSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coronavirustrackersite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronaVirusTrackerSiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
